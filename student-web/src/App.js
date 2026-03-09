57b75dddefc69ee2381c3e5295c9d8c70539c1e6
Ephraim Basubi <basubiephrem@gmail.com>
2026-03-09 08:24:26 +0300

    changes font family and hero section

--- student-web/src/App.js
+++ student-web/src/App.js
@@ -30,7 +30,7 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    fontFamily: '"Poppins", "Roboto", "Helvetica", "Arial", sans-serif',
     h1: {
       fontSize: '2rem', // 32px
       fontWeight: 700,
